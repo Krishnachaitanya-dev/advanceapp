505168d05bc9d835bb2d6326bcb6d49ca32c3614
krishnachaitanyaBW <krishna_chaitanya@thebusinesswings.com>
2025-07-28 14:24:55 +0530

    Add files via upload

---
+++ src/pages/PublicTermsOfService.tsx
@@ -0,0 +1,8 @@
+
+import PublicTermsOfServicePage from '@/components/PublicTermsOfServicePage';
+
+const PublicTermsOfService = () => {
+  return <PublicTermsOfServicePage />;
+};
+
+export default PublicTermsOfService;
